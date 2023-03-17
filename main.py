64fec84d16f42fc054842e339ba6e93356065554
Abigail Haddad <abigail.haddad@gmail.com>
2023-03-16 21:27:16 -0400

    cleaned up and documented by GPT-4, but same results

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -17,75 +9,105 @@ from urllib3 import disable_warnings
 disable_warnings(InsecureRequestWarning)
 
 
+def get_soup(url: str) -> BeautifulSoup:
+    """
+    Get the soup object of the given URL.
 
-disable_warnings(InsecureRequestWarning)
-
-page = requests.get('http://facebook.com', verify=False)
-
-
-def get_soup(url):
+    :param url: URL of the webpage.
+    :return: Soup object of the webpage.
+    """
     reqs = requests.get(url, verify=False)
     soup = BeautifulSoup(reqs.text, 'html.parser')
-    return(soup)
-    
-def get_links(soup):
-    urlDict={}
+    return soup
+
+
+def get_links(soup: BeautifulSoup) -> dict:
+    """
+    Extract all the links from the soup object and return them in a dictionary.
+
+    :param soup: Soup object of the webpage.
+    :return: Dictionary containing the text and link of each <a> element.
+    """
+    url_dict = {}
     for link in soup.find_all('a'):
-        urlOnPage=link.get('href')
-        text=link.text
-        urlDict[text]=urlOnPage
-    return(urlDict)
-    
-def filter_attachments(pair):
-    key, value = pair
-    if "attachments" in value:
-        return True  # keep pair in the filtered dictionary
-    else:
-        return False  # filter pair out of the dictionary
-    
-def file_type(link):
-    extension=link.split(".")[-1]
-    return(extension)
+        url_on_page = link.get('href')
+        text = link.text
+        url_dict[text] = url_on_page
+    return url_dict
 
-def takeUrlGetResults(url):
-    time.sleep(1) 
-    try:
-        soup=get_soup(url)
-    except:
-        print(url)
-    for item in soup.find_all('title'):
-        title=item.get_text()
-    try:
-        content = soup.find('meta', {'name':'description'}).get('content')
-    except:
-        content="missing"
-    print(url)
-    urlDict=get_links(soup)
-    filtered_urls= dict(filter(filter_attachments, urlDict.items()))
-    df=pd.DataFrame()
-    df['Name']=filtered_urls.keys()
-    df['Link']=filtered_urls.values()
-    try:
-        df['Title']=title
-    except:
-        df['Title']="missing"
-    df['Extension']=df['Link'].apply(file_type)
-    df['Page Content']=content
-    return(df)
 
-def crawl_site(url, depth=3):
-    # this piece starts at a website and gets the other links on those, and does recursion
-    # stop if it's not at dc.gov
+def filter_attachments(pair: tuple) -> bool:
+    """
+    Check if the "attachments" keyword is present in the value of the input tuple.
+
+    :param pair: Tuple containing key and value.
+    :return: True if "attachments" is present in the value, False otherwise.
+    """
+    _, value = pair
+    return "attachments" in value
+
+
+def file_type(link: str) -> str:
+    """
+    Get the file extension from the input link.
+
+    :param link: URL of the file.
+    :return: File extension.
+    """
+    extension = link.split(".")[-1]
+    return extension
+
+
+def take_url_get_results(url: str) -> pd.DataFrame:
+    """
+    Scrape the input URL and return a dataframe containing the relevant information.
+
+    :param url: URL of the webpage.
+    :return: Dataframe containing the scraped information.
+    """
+    time.sleep(1)
+    try:
+        soup = get_soup(url)
+    except Exception as e:
+        print(f"Error for URL {url}: {e}")
+        return pd.DataFrame()
+
+    title = soup.find('title').get_text() if soup.find('title') else "missing"
+    content = soup.find('meta', {'name': 'description'}).get('content') if soup.find('meta', {'name': 'description'}) else "missing"
+
+    url_dict = get_links(soup)
+    filtered_urls = dict(filter(filter_attachments, url_dict.items()))
+
+    df = pd.DataFrame()
+    df['Name'] = filtered_urls.keys()
+    df['Link'] = filtered_urls.values()
+    df['Title'] = title
+    df['Extension'] = df['Link'].apply(file_type)
+    df['Page Content'] = content
+    return df
+
+
+def crawl_site(url: str, depth: int = 3) -> set:
+    """
+    Crawl the given website and return the visited URLs.
+
+    :param url: Starting URL.
+    :param depth: Depth of the crawl.
+    :return: Set of visited URLs.
+    """
     visited_urls = set()
-    def crawl(url, depth):
+
+    def crawl(url: str, depth: int):
+        nonlocal visited_urls
         if depth == 0:
             return
         if url in visited_urls:
             return
         visited_urls.add(url)
         try:
-            response = requests.get(url)
-        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            response = requests.get(url, verify=False)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError) as e:
+            print(f"Error for URL {url}: {e}")
             return
         soup = BeautifulSoup(response.text, 'html.parser')
         for link in soup.find_all('a'):
@@ -94,15 +116,17 @@ def crawl_site(url, depth=3):
                 if href:
                     href = urljoin(url, href)
                     crawl(href, depth - 1)
-            except:
-                pass
+            except Exception as e:
+                print(f"Error for link {link}: {e}")
+
     crawl(url, depth)
     return visited_urls
 
- 
-visited_urls=crawl_site("https://dcps.dc.gov/service/school-data", depth=3)
-just_dc_urls=[i for i in visited_urls if i.startswith("https://dcps.dc.gov")]
-subdfs=[takeUrlGetResults(url) for url in just_dc_urls]
-df=pd.concat(subdfs)
+
+visited_urls = crawl_site("https://dcps.dc.gov/service/school-data", depth=3)
+just_dc_urls = [i for i in visited_urls if i.startswith("https://dcps.dc.gov")]
+sub_dfs = [take_url_get_results(url) for url in just_dc_urls]
+df = pd.concat(sub_dfs, ignore_index=True)
 df.to_csv("sampleResults.csv")
 pd.DataFrame(just_dc_urls).to_csv("urls.csv")
+
